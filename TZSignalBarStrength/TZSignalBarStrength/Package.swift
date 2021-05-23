2dc9778cb12508b17440b83f5f1412ad2707f31b
Rana Hossam <ranahossam263@gmail.com>
2021-05-23 18:05:00 +0200

    extract xcframework

--- TZSignalBarStrength/TZSignalBarStrength/Package.swift
+++ TZSignalBarStrength/TZSignalBarStrength/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "TZSignalBarStrength", path: "TZSignalBarStrength.xcframework")
+//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "TZSignalBarStrength", path: "./Sources/TZSignalBarStrength.xcframework")
     ]
 )
